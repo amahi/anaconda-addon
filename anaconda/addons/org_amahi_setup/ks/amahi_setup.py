6191bb0d64f93a97dd52da62afd1cd878d0f8311
Sanjit <sujitsanjit@gmail.com>
2018-05-18 19:48:31 +0530

    Removed unnecessary commands

--- anaconda/addons/org_amahi_setup/ks/amahi_setup.py
+++ anaconda/addons/org_amahi_setup/ks/amahi_setup.py
@@ -170,7 +170,6 @@ class AmahiData(AddonData):
         call("echo 'rm -rf /etc/systemd/system/getty@tty1.service.d ' >> "+normalpath+"/usr/bin/hda-install-script.sh", shell=True)
         call("echo 'dhclient && dnf -y swap fedora-release generic-release' >> "+normalpath+"/usr/bin/hda-install-script.sh", shell=True)
         call("echo 'mv -f /usr/bin/issue /usr/bin/issue.net /etc' >> "+normalpath+"/usr/bin/hda-install-script.sh", shell=True)
-        call("echo 'systemctl enable httpd.service ' >> "+normalpath+"/usr/bin/hda-install-script.sh", shell=True)
         call("echo 'systemctl disable amahi_setup.service && reboot ' >> "+normalpath+"/usr/bin/hda-install-script.sh", shell=True)
  
         
