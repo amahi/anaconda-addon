367f23e158c760c6423822221be2bbe52202267b
Sanjit <sujitsanjit@gmail.com>
2018-06-03 17:35:47 +0530

    removed whitespaces

--- anaconda/addons/org_amahi_setup/hda-install-script.sh
+++ anaconda/addons/org_amahi_setup/hda-install-script.sh
@@ -2,33 +2,25 @@
 
 #This will keep executing the command untill the previous command exit status is 0
 while true
-
 do
-       dnf -y install freeglut-devel
-       
+       dnf -y install hda-ctl
        if [ $? != 0 ]; then
                    killall dhclient && dhclient
                    continue
        fi
        break
-
 done
-
 sleep 10
 
 #This will keep executing the command untill the previous command exit status is 0
 while true
-
 do
        hda-install 
-
-       
        if [ $? != 0 ]; then
                    killall dhclient && dhclient
                    continue
        fi
        break
-
 done
 
 #to remove that 'Amahi server setup' message during startup
@@ -36,17 +28,13 @@ rm -rf /etc/systemd/system/getty@tty1.service.d
 
 #This will keep executing the command untill the previous command exit status is 0
 while true
-
 do
        dhclient && dnf -y swap fedora-release generic-release
-
        if [ $? != 0 ]; then
                    killall dhclient && dhclient
                    continue
-
        fi
        break
-
 done
 
 #to copy the modified 'issue' for custom amahi message during Login prompt
