0f56808594c4c510747df2007a6039219248fdd2
Sanjit <sujitsanjit@gmail.com>
2018-06-14 23:30:02 +0530

    Reorder commands in server setup script

--- anaconda/addons/org_amahi_setup/hda-install-script.sh
+++ anaconda/addons/org_amahi_setup/hda-install-script.sh
@@ -15,6 +15,17 @@ sleep 10
 #This will keep executing the command untill the previous command exit status is 0
 while true
 do
+       dnf -y swap fedora-release generic-release
+       if [ $? != 0 ]; then
+                   killall dhclient && dhclient
+                   continue
+       fi
+       break
+done
+
+#This will keep executing the command untill the previous command exit status is 0
+while true
+do
        hda-install 
        if [ $? != 0 ]; then
                    killall dhclient && dhclient
@@ -25,17 +36,6 @@ done
 
 #to remove that 'Amahi server setup' message during startup
 rm -rf /etc/systemd/system/getty@tty1.service.d
-
-#This will keep executing the command untill the previous command exit status is 0
-while true
-do
-       dhclient && dnf -y swap fedora-release generic-release
-       if [ $? != 0 ]; then
-                   killall dhclient && dhclient
-                   continue
-       fi
-       break
-done
 
 #to copy the modified 'issue' for custom amahi message during Login prompt
 mv -f /usr/bin/issue /usr/bin/issue.net /etc
